59d0a1be92de0f4816c525dad5fc329f52820fee
Reny Bajracharya <renynet7@gmail.com>
2021-01-11 13:15:22 +1100

    holidayAPI in api.js

--- js/api.js
+++ js/api.js
@@ -2,34 +2,9 @@
 //const appId = "ebff93434917c879197142ac42340760";
 
 // getDataForCity function that fetches weather info from openweathermap api
-const localTime = document.querySelector("#localTime");
+//const localTime = document.querySelector("#localTime");
 
-const getLocalTime = () =>
-  fetch(
-    `https://api.xmltime.com/timeservice?accesskey=Yt7QB7oKxz&expires=2021-01-07T04%3A26%3A15%2B00%3A00&signature=lTB5vxy7YxNUGSn2yHAq1Uzuy2w%3D&version=3&placeid=750`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // get the data we need for our html from the response
-      const name = data.locations[0].time.iso;
-     
-      //const emoji = emojis[data.weather[0].icon];
-     // const temp = Math.round(data.main.temp);
-      //const feelsLike = Math.round(data.main.feels_like);
-      //const description = data.weather[0].description;
-
-      // create the card html
-    //   const cardHtml = createCardHtml(
-    //     name
-    //   );
-
-      // render!
-     localTime.innerHTML = name;
-    })
-    .catch((error) => {
-      localTime.innerHTML = `<em>Server returned error: "${error.message}".</em>`;
-    });
-    getLocalTime();
+//
 // selecting all the things needed
 
 // event listener for a click event on the "Go!" button
@@ -39,4 +14,30 @@ const getLocalTime = () =>
 //   // get the weather data for the city
 //   getDataForCity(city);
 // });
+//Holiday API
+//let newTaskDueDate = document.querySelector('#newTaskDueDate');
+let holidayMsg = document.querySelector("#holidayMsg");
+newTaskDate.addEventListener("blur", checkHoliday);
 
+function checkHoliday() {
+  console.log(newTaskDate.value);
+  //let day = 2;
+  //let month = 01;
+  //let year = 2021;
+
+  fetch(
+    `https://holidays.abstractapi.com/v1/?api_key=594f8fb8f98b4c91a8db10f4f3d2d6c8&country=AU&year=2021&month=01&day=26`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // get the data we need for our html from the response
+      const name = data[0].name;
+      console.log(name);
+      holidayMsg.innerHTML = `The chosen date is a public Holiday - ${name}`;
+      holidayMsg.style.color = 'red';
+      holidayMsg.style.fontWeight = 'bold';
+    })
+    .catch((error) => {
+      holidayMsg.innerHTML = " ";
+    });
+}
